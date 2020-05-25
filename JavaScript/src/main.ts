4d9cbf96cf19216e84c2250d8b0aff051cbb2559
Kevin Eady <8634912+KevinEady@users.noreply.github.com>
2020-05-25 18:17:48 +0200

    Introduce EscriptLooseParser

--- JavaScript/src/main.ts
+++ JavaScript/src/main.ts
@@ -1,10 +1,16 @@
 import { EscriptLexer } from './antlr/EscriptLexer';
-export * from './antlr/EscriptParser';
+export * as strict from './antlr/EscriptParser';
+export * as loose from './antlr/EscriptLooseParser';
+
 import type { EscriptParserListener } from './antlr/EscriptParserListener';
 import type { EscriptParserVisitor } from './antlr/EscriptParserVisitor';
+import type { EscriptLooseParserListener } from './antlr/EscriptLooseParserListener';
+import type { EscriptLooseParserVisitor } from './antlr/EscriptLooseParserVisitor';
 
 export type { EscriptParserListener };
 export type { EscriptParserVisitor };
+export type { EscriptLooseParserListener };
+export type { EscriptLooseParserVisitor };
 
 export {
     EscriptLexer
