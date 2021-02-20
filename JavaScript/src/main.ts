782f464f47350ce8418cfbc0588acc9037784cab
Kevin Eady <8634912+KevinEady@users.noreply.github.com>
2021-02-20 23:41:52 +0100

    add CaseChangingStream

--- JavaScript/src/main.ts
+++ JavaScript/src/main.ts
@@ -2,3 +2,4 @@ export * from './antlr/EscriptLexer';
 export * from './antlr/EscriptParser';
 export * from './antlr/EscriptParserListener';
 export * from './antlr/EscriptParserVisitor';
+export * from './case-changer';
